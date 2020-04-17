5fbd1926d3b35bc5efeddfe8f672e789dfdd5389
Henrique <ricci.henrique@outlook.com>
2020-04-17 15:47:00 -0300

    Correção no teste

--- TesteSoftware/test/entidades/AcertoTest.java
+++ TesteSoftware/test/entidades/AcertoTest.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entidades;
 
 import banco.Banco;
-import banco.Conexao;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import junitparams.FileParameters;
 import junitparams.JUnitParamsRunner;
 import org.junit.After;
@@ -19,13 +13,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
-
-/**
- *
- * @author Alexandre
- */
 
 @RunWith(JUnitParamsRunner.class)
 public class AcertoTest {
@@ -52,14 +40,33 @@ public class AcertoTest {
         Banco.getCon().getConnection().rollback();
         Banco.getCon().Disconnect();
     }
+    
+    
+    private boolean verifica(Acerto a1, Acerto a2) {
+        return a1.getData().equals(a2.getData()) && a1.getValor() == a2.getValor() && a1.isTipo() == a2.isTipo() && a1.getMotivo().equals(a2.getMotivo());
+    }
 
-    /**
-     * Test of insere method, of class Acerto.
-     */
     @Test
     @FileParameters("test/entidades/acerto.csv")
-    public void testInsere(String data, double valor, boolean tipo, String motivo, boolean resultado) {
-        Acerto a = new Acerto(LocalDate.parse(data, DateTimeFormatter.ISO_DATE),valor, tipo, motivo);
-        Assert.assertEquals(resultado,a.insere());
+    public void testInsere(String data, double valor, boolean tipo, String motivo, boolean resultado) throws SQLException 
+    {
+        Acerto a = new Acerto(LocalDate.parse(data, DateTimeFormatter.ISO_DATE), valor, tipo, motivo);
+        boolean inseriu = a.insere(); // Insere no banco e, caso insira, retorna true
+        Assert.assertEquals(resultado, inseriu); // Verifica se era para ser inserido
+        if(inseriu) // A partir daqui, verifica se os dados que foram para o banco são os mesmo passados
+        {
+            ResultSet rs = Banco.getCon().consultar("select * from Acerto ORDER BY \"AceCod\" DESC LIMIT 1"); // Seleciona o ultimo registro inserido
+            if(rs.next()) 
+            {
+                Acerto a2 = new Acerto(rs.getDate("data").toLocalDate(), rs.getDouble("valor"), rs.getBoolean("tipo"), rs.getString("motivo")); // Instancia um novo objeto com os valores do banco
+                Assert.assertEquals(true, verifica(a, a2)); // verifica se os valores são iguais nos dois objetos
+                
+                /*
+                    Chico, não fizemos casos de teste no CSV em que faltam parametros, pois a pŕopria biblioteca que converte o CSV trata isso, 
+                    então sempre daria erro. Colocar valores errados, por exemplo, a data, causa erro no parse, e também é informado automaticamente.
+                    Por fim, não enviar o motivo, por exemplo, insere '' no banco, e não null. Sendo assim, para o banco de dados, '' é um valor válido.
+                */
+            }
+        }
     }
 }
